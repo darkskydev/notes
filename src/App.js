281f6fb66929c96ddd462dcc9dd0ccf5acfaeb6a
Jeff Huston <jeff.huston@fiserv.com>
2020-04-14 06:37:41 -0500

    Editing state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react'
-import Notes from './tables/Notes'
+import React, { useState, Fragment } from 'react'
 import AddNoteForm from './forms/AddNoteForm'
-import EditNoteForm from './forms/EditNoteForm'
+import EditNoteForm from './forms/EditeNoteForm'
+import Notes from './tables/Notes'
 
 const App = () => {
+    // Data
     const usersData = [
         { id: 1, name: 'Tania', username: 'floppydiskette' },
         { id: 2, name: 'Craig', username: 'siliconeidolon' },
         { id: 3, name: 'Ben', username: 'benisphere' },
     ]
 
-    const [users, setUsers] = useState(usersData)
-    const [editing, setEditing] = useState(false)
     const initialFormState = { id: null, name: '', username: '' }
-    const [currentUser, setCurrentUser] = useState(initialFormState)
 
+    // Setting state
+    const [ users, setUsers ] = useState(usersData)
+    const [ currentUser, setCurrentUser ] = useState(initialFormState)
+    const [ editing, setEditing ] = useState(false)
+
+    // CRUD operations
     const addUser = user => {
         user.id = users.length + 1
-        setUsers([...users, user])
+        setUsers([ ...users, user ])
     }
 
     const deleteUser = id => {
+        setEditing(false)
+
         setUsers(users.filter(user => user.id !== id))
-    }
-
-    const editRow = user => {
-        setEditing(true)
-
-        setCurrentUser({ id: user.id, name: user.name, username: user.username })
     }
 
     const updateUser = (id, updatedUser) => {
@@ -36,23 +36,39 @@ const App = () => {
         setUsers(users.map(user => (user.id === id ? updatedUser : user)))
     }
 
+    const editRow = user => {
+        setEditing(true)
+
+        setCurrentUser({ id: user.id, name: user.name, username: user.username })
+    }
+
     return (
-        <div className="flex-large">
-            {editing ? (
-                <div>
-                    <h2>Edit user</h2>
-                    <EditNoteForm
-                        setEditing={setEditing}
-                        currentUser={currentUser}
-                        updateUser={updateUser}
-                    />
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+                    {editing ? (
+                        <Fragment>
+                            <h2>Edit user</h2>
+                            <EditNoteForm
+                                editing={editing}
+                                setEditing={setEditing}
+                                currentUser={currentUser}
+                                updateUser={updateUser}
+                            />
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <h2>Add user</h2>
+                            <AddNoteForm addUser={addUser} />
+                        </Fragment>
+                    )}
                 </div>
-            ) : (
-                <div>
-                    <h2>Add user</h2>
-                    <AddNoteForm addUser={addUser} />
+                <div className="flex-large">
+                    <h2>View users</h2>
+                    <Notes users={users} editRow={editRow} deleteUser={deleteUser} />
                 </div>
-            )}
+            </div>
         </div>
     )
 }
